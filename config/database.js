7c131645c7be4c0b0b03ad0a19c0b53f054fabf6
coreytegeler <coreytegeler@gmail.com>
2016-08-02 11:09:49 -0400

    hide mongo pw

--- config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	development: 'localhost:27017/civil-rights',
-	test: 'localhost:27017/civil-rights',
-	production: 'mongodb://heroku_9lc8j8h8:TY5sKAFZT-3ijkBOj2hn8luf5Y9@ds029735.mlab.com:29735/heroku_9lc8j8h8'
-};
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var session      = require('express-session');
 var db = {
 	development: 'localhost:27017/civil-rights',
 	test: 'localhost:27017/civil-rights',
-	production: 'mongodb://heroku_9lc8j8h8:TY5sKAFZT-3ijkBOj2hn8luf5Y9@ds029735.mlab.com:29735/heroku_9lc8j8h8'
+	production: process.env.MONGOLAB_URI
 };
 
 app.set('dbUrl', db[app.settings.env]);
