23f6357145d2d15fe618e75036004ad07862b08c
coreytegeler <coreytegeler@gmail.com>
2016-08-02 09:57:32 -0400

    port

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
+app.set('port', port);
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 app.locals.uppercase = function(value){
